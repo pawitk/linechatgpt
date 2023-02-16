30f6fdd0d808dcd16b2c4ebef74acf50b4c9007d
Pawit Khid-arn <khidarn.pawit@gmail.com>
2023-02-16 11:58:32 +0900

    added openai and reply integration

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -10,15 +10,24 @@ class WebhookController < ApplicationController
     message = event['message']['text']
     reply_token = event['replyToken']
 
-    @resource = RestClient::Resource.new('https://api.openai.com/v1/completions')
-    @response = @resource.post({
-      "model" => "text-davinci-003",
-      "prompt" => message,
-      "max_tokens" => 20,
-      "temperature" => 0
-    }, :Authorization => "Bearer #{Rails.application.credentials.openai_api_key}" )
+    @request_body = {"model" => "text-davinci-003","prompt" => message,"max_tokens" => 20,"temperature" => 0}
+    @response = RestClient.post('https://api.openai.com/v1/completions',@request_body.to_json,:content_type => 'application/json', :authorization => "Bearer #{Rails.application.credentials.openai_api_key}")
 
-    outgoing = OutgoingMessage.create(incoming_message: incoming, body: @response.body.to_json)
+    outgoing = OutgoingMessage.create(incoming_message: incoming, body: @response.body)
+
+    @response_json = JSON.parse(@response.body)
+    @response_message = @response_json['choices'].first['text']
+
+    @reply_body = {
+      "replyToken" => reply_token,
+      "messages" => [
+          {
+              "type" => "text",
+              "text" => @response_message.strip
+          }
+      ]
+    }
+    RestClient.post('https://api.line.me/v2/bot/message/reply',@reply_body.to_json,,:content_type => 'application/json', :authorization => "Bearer #{Rails.application.credentials.line_channel_access_token}")
 
     render json: true
   end
